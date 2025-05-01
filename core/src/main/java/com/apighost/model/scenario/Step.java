8c85d7475a55e7414762f930557a4a45a4ad00b1
김성준 <sjkim2243@gmail.com>
2025-05-01 11:05:53 +0900

    feat: create test scenario YAML model

--- core/src/main/java/com/apighost/model/scenario/Step.java
+++ core/src/main/java/com/apighost/model/scenario/Step.java
@@ -6,48 +6,63 @@ import java.util.Map;
 
 public class Step {
 
-    /**
-     * HTTP or WEBSOCKET
-     */
     private String type;
-
-    /**
-     * position of diagram on GUI
-     */
     private Position position;
-
     private Request request;
     private List<Response> response;
 
-    public String getType() {
-        return type;
+    private Step(Builder builder) {
+        this.type = builder.type;
+        this.position = builder.position;
+        this.request = builder.request;
+        this.response = builder.response;
     }
 
-    public void setType(String type) {
-        this.type = type;
+    public static class Builder {
+
+        private String type;
+        private Position position;
+        private Request request;
+        private List<Response> response;
+
+        public Builder type(String type) {
+            this.type = type;
+            return this;
+        }
+
+        public Builder position(Position position) {
+            this.position = position;
+            return this;
+        }
+
+        public Builder request(Request request) {
+            this.request = request;
+            return this;
+        }
+
+        public Builder response(List<Response> response) {
+            this.response = response;
+            return this;
+        }
+
+        public Step build() {
+            return new Step(this);
+        }
+    }
+
+    public String getType() {
+        return type;
     }
 
     public Position getPosition() {
         return position;
     }
 
-    public void setPosition(Position position) {
-        this.position = position;
-    }
-
     public Request getRequest() {
         return request;
     }
 
-    public void setRequest(Request request) {
-        this.request = request;
-    }
-
     public List<Response> getResponse() {
         return response;
-    }
-
-    public void setResponse(List<Response> response) {
-        this.response = response;
     }
 }
--- core/src/main/java/com/apighost/model/scenario/When.java
+++ core/src/main/java/com/apighost/model/scenario/When.java
@@ -8,35 +8,53 @@ public class When {
      * status of response ex. "200-299" or 200
      */
     private Object status;
-
     private Map<String, Object> body;
-
     /**
      * Conditional expression (ex: "${response.body.posts.length} == 0")
      */
     private String condition;
 
-    public Object getStatus() {
-        return status;
+    private When(Builder builder) {
+        this.status = builder.status;
+        this.body = builder.body;
+        this.condition = builder.condition;
     }
 
-    public void setStatus(Object status) {
-        this.status = status;
+    public static class Builder {
+
+        private Object status;
+        private Map<String, Object> body;
+        private String condition;
+
+        public Builder status(Object status) {
+            this.status = status;
+            return this;
+        }
+
+        public Builder body(Map<String, Object> body) {
+            this.body = body;
+            return this;
+        }
+
+        public Builder condition(String condition) {
+            this.condition = condition;
+            return this;
+        }
+
+        public When build() {
+            return new When(this);
+        }
+    }
+
+    public Object getStatus() {
+        return status;
     }
 
     public Map<String, Object> getBody() {
         return body;
     }
 
-    public void setBody(Map<String, Object> body) {
-        this.body = body;
-    }
-
     public String getCondition() {
         return condition;
-    }
-
-    public void setCondition(String condition) {
-        this.condition = condition;
     }
 }
