dc0ed0259fdb2d4c23dbf809bce063e326bf8952
haazz <gkwldnjs121@gmail.com>
2025-05-02 10:36:06 +0900

    feat: Create ScenrioResultReader interface

---
+++ parser/src/main/java/com/apighost/parser/scenario/reader/ScenarioResultReader.java
@@ -0,0 +1,22 @@
+package com.apighost.parser.scenario.reader;
+
+import com.apighost.model.scenario.result.ScenarioResult;
+import java.io.IOException;
+
+/**
+ * Interface for reading {@link ScenarioResult} objects from external file.
+ *
+ * @author haazz
+ * @version BETA-0.0.1
+ */
+public interface ScenarioResultReader {
+
+    /**
+     * Reads a ScenarioResult from the given file path.
+     *
+     * @param filePath path to the result file
+     * @return deserialized ScenarioResult object
+     * @throws IOException if reading or parsing fails
+     */
+    ScenarioResult readScenarioResult(String filePath) throws IOException;
+}
