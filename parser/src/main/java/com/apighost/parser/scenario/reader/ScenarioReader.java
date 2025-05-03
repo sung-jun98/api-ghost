0f7984d801ad91a20b509c14f8895fe9fe3a4e57
김성준 <sjkim2243@gmail.com>
2025-05-03 13:32:09 +0900

    fix: Change of data structure steps of ScenarioDto
    Replaced the generic Map used for managing steps in ScenarioDto with a HashMap implementation.

--- parser/src/main/java/com/apighost/parser/scenario/reader/ScenarioReader.java
+++ parser/src/main/java/com/apighost/parser/scenario/reader/ScenarioReader.java
@@ -1,9 +1,23 @@
 package com.apighost.parser.scenario.reader;
 
 import com.apighost.model.scenario.Scenario;
+import com.apighost.model.scenario.ScenarioResult;
 import java.io.IOException;
 
+/**
+ * Interface for reading {@link Scenario} objects from external file.
+ *
+ * @author sung-jun98
+ * @version BETA-0.0.1
+ */
 public interface ScenarioReader {
-    public Scenario readScenario (String yamlFilePath) throws IOException;
+    /**
+     * Reads a Scenario from the given file path.
+     *
+     * @param filePath path to the result file
+     * @return deserialized Scenario object
+     * @throws IOException if reading or parsing fails
+     */
+    public Scenario readScenario (String filePath) throws IOException;
 
 }
--- parser/src/main/java/com/apighost/parser/scenario/reader/YamlScenarioReader.java
+++ parser/src/main/java/com/apighost/parser/scenario/reader/YamlScenarioReader.java
@@ -1,32 +1,48 @@
 package com.apighost.parser.scenario.reader;
 
 import com.apighost.model.scenario.Scenario;
+import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
 import java.io.File;
 import java.io.IOException;
 
+/**
+ * JSON implementation of {@link YamlScenarioReader}.
+ *
+ * <p>Example usage:</p>
+ * <pre>
+ *     new YamlScenarioReader().readScenario("filPath");
+ * </pre>
+ *
+ * @author sun-jun98
+ * @version BETA-0.0.1
+ */
 public class YamlScenarioReader implements ScenarioReader {
+
+    ObjectMapper objectMapper;
+
+    public YamlScenarioReader() {
+        objectMapper = new ObjectMapper(new YAMLFactory());
+        this.objectMapper.disable(MapperFeature.SORT_PROPERTIES_ALPHABETICALLY);
+        this.objectMapper.disable(SerializationFeature.ORDER_MAP_ENTRIES_BY_KEYS);
+    }
 
     /**
      * Convert the YAML scenario information stored in the local directory to DTO
      *
-     * @param yamlFilePath The path of the YAML file to be read
-     * @return Scenario Scenario information converted to DTO
-     * @throws IOException
+     * @param filePath The path of the YAML file to be read
+     * @return {@link Scenario} Scenario information converted to DTO
+     * @throws IOException if the file is not found, invalid, or cannot be parsed
      */
     @Override
-    public Scenario readScenario(String yamlFilePath) throws IOException {
-        ObjectMapper objectMapper = new ObjectMapper(new YAMLFactory());
-
-        File yamlFile = new File(yamlFilePath);
-        try {
-            Scenario scenario = objectMapper.readValue(yamlFile, Scenario.class);
-
-            return scenario;
-        } catch (IOException e) {
-            return null;
+    public Scenario readScenario(String filePath) throws IOException {
+        File inputFile = new File(filePath);
+        if (!inputFile.isFile()) {
+            throw new IOException("Invalid file path: " + filePath);
         }
+        return objectMapper.readValue(inputFile, Scenario.class);
     }
 }
