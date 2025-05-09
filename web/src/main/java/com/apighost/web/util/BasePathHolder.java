aebbfabb33c4dfa1cca42934e1f5a7a2bcfa95c2
haazz <gkwldnjs121@gmail.com>
2025-05-09 20:19:20 +0900

    feat: Create BasePathHolder
    Designed with singlton pattern
    Default value is OS user.home

---
+++ web/src/main/java/com/apighost/web/util/BasePathHolder.java
@@ -0,0 +1,26 @@
+package com.apighost.web.util;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class BasePathHolder {
+    private static final BasePathHolder INSTANCE = new BasePathHolder();
+
+    private Path basePath;
+
+    private BasePathHolder() {
+        this.basePath = Paths.get(System.getProperty("user.home"));
+    }
+
+    public static BasePathHolder getInstance() {
+        return INSTANCE;
+    }
+
+    public Path getBasePath() {
+        return basePath;
+    }
+
+    public void setBasePath(Path basePath) {
+        this.basePath = basePath;
+    }
+}
