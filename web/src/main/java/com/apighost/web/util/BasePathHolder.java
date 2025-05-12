30429cd13645857f0c8424d5e936267d66c5da24
haazz <gkwldnjs121@gmail.com>
2025-05-12 09:47:23 +0900

    refactor: Update lazy initialization of singleton

--- web/src/main/java/com/apighost/web/util/BasePathHolder.java
+++ web/src/main/java/com/apighost/web/util/BasePathHolder.java
@@ -4,16 +4,18 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class BasePathHolder {
-    private static final BasePathHolder INSTANCE = new BasePathHolder();
-
     private Path basePath;
 
     private BasePathHolder() {
         this.basePath = Paths.get(System.getProperty("user.home"));
     }
 
+    private static class Holder {
+        private static final BasePathHolder INSTANCE = new BasePathHolder();
+    }
+
     public static BasePathHolder getInstance() {
-        return INSTANCE;
+        return Holder.INSTANCE;
     }
 
     public Path getBasePath() {
