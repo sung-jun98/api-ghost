0b4e346607972d1bf37d15873a376456e40315f2
김성준 <sjkim2243@gmail.com>
2025-05-16 19:42:31 +0900

    feat: create ANSI escape logic

---
+++ cli/src/main/java/com/apighost/cli/util/console/AnsiEscapeUtil.java
@@ -0,0 +1,23 @@
+package com.apighost.cli.util.console;
+
+/**
+ * Util defining the escape variable mainly used in Picocli's ANSI ESCAPE
+ *
+ * @author sung-jun98
+ * @version BETA-0.0.1
+ */
+public class AnsiEscapeUtil {
+
+    public static final String ESC = "\u001B";
+    public static final String CSI = ESC + "[";
+
+    public static final String CURSOR_POSITION = CSI + "%d;%dH";
+    public static final String CLEAR_LINE = CSI + "2K";
+    public static final String CLEAR_SCREEN = CSI + "2J";
+
+    public static String setCursorPosition(int row, int column) {
+        return String.format(CURSOR_POSITION, row, column);
+    }
+
+
+}
