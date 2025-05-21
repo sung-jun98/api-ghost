7c6d38a18488654adb23d7824ed98c87446ff769
haazz <gkwldnjs121@gmail.com>
2025-05-21 11:17:08 +0900

    fix: fix when write on linux error

--- cli/src/main/java/com/apighost/cli/util/console/AnsiEscapeUtil.java
+++ cli/src/main/java/com/apighost/cli/util/console/AnsiEscapeUtil.java
@@ -13,7 +13,7 @@ public class AnsiEscapeUtil {
 
     public static final String CURSOR_POSITION = CSI + "%d;%dH";
     public static final String CLEAR_LINE = CSI + "2K";
-    public static final String CLEAR_SCREEN = CSI + "2J";
+    public static final String CLEAR_SCREEN = "\033[H\033[2J";
 
     public static final String SAVE_CURSOR_POSITION = CSI + "s";
     public static final String RESTORE_CURSOR_POSITION = CSI + "u";
